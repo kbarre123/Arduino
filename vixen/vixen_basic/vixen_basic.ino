8b5fb7eca5a78c7ec2f0c5c918982be864bb141d
KC Barrett <kbarre123@gmail.com>
2013-11-10 10:50:58 -0600

    Streamline "basic" code.
    Added for-loops and defined "channel count" and "baud rate" variables to
    streamline the code.

--- vixen/vixen_basic/vixen_basic.ino
+++ vixen/vixen_basic/vixen_basic.ino
@@ -1,64 +1,51 @@
-int A = 2;
-int B = 3;
-int C = 4;
-int D = 5;
-int E = 6;
-int F = 7;
-int G = 8;
-int H = 9;
-int I = 10;
-int J = 11;
-int K = 12;
-int L = 13;
-int M = A0;
-int N = A1;
+int A = A4;
+int B = A5;
+int C = 2;
+int D = 3;
+int E = 4;
+int F = 5;
+int G = 6;
+int H = 7;
+int I = 8;
+int J = 9;
+int K = 10;
+int L = 11;
+int M = 12;
+int N = 13;
+
+int CHANNEL_COUNT = 14;
+
+int channels[] = 
+{
+  A, B, C, D, E, F, G, H, I, J, K, L, M, N
+};
 
 int i = 0;
 int incomingByte[14];
+int BAUD_RATE = 57600;
 
 void setup()
 {
-  Serial.begin(57600);
+  Serial.begin(BAUD_RATE);
 
-  pinMode(A, OUTPUT);
-  pinMode(B, OUTPUT);
-  pinMode(C, OUTPUT);
-  pinMode(D, OUTPUT);
-  pinMode(E, OUTPUT);
-  pinMode(F, OUTPUT);
-  pinMode(G, OUTPUT);
-  pinMode(H, OUTPUT);
-  pinMode(I, OUTPUT);
-  pinMode(J, OUTPUT);
-  pinMode(K, OUTPUT);
-  pinMode(L, OUTPUT);
-  pinMode(M, OUTPUT);
-  pinMode(N, OUTPUT);
-
+  for(int i = 0; i < CHANNEL_COUNT; i++)
+  {
+    pinMode(channels[i], OUTPUT);  
+  }
 }
 
 void loop()
 {
-  if (Serial.available() >= 14)
+  if (Serial.available() >= CHANNEL_COUNT)
   {
-    for (int i=0; i<14; i++)
+    for (int i=0; i<CHANNEL_COUNT; i++)
     {
       incomingByte[i] = Serial.read();
-    }                                       // Arduino pins
-    digitalWrite(A, incomingByte[0]);  // Pin 2
-    digitalWrite(B, incomingByte[1]);  // Pin 3
-    digitalWrite(C, incomingByte[2]);  // Pin 4
-    digitalWrite(D, incomingByte[3]);  // Pin 5
-    digitalWrite(E, incomingByte[4]);  // Pin 6
-    digitalWrite(F, incomingByte[5]);  // Pin 7
-    digitalWrite(G, incomingByte[6]);  // Pin 8
-    digitalWrite(H, incomingByte[7]);  // Pin 9
-    digitalWrite(I, incomingByte[8]);  // Pin 10
-    digitalWrite(J, incomingByte[9]);  // Pin 11
-    digitalWrite(K, incomingByte[10]); // Pin 12
-    digitalWrite(L, incomingByte[11]); // Pin 13
-    digitalWrite(M, incomingByte[12]); // Pin A0
-    digitalWrite(N, incomingByte[13]); // Pin A1
+    }                                  
     
+    for (int i=0; i<CHANNEL_COUNT; i++)
+    {
+      digitalWrite(channels[i], incomingByte[i]);
+    }
   }
 }
