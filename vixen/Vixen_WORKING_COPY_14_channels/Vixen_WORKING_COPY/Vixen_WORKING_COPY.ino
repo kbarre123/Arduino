d3927832f2e3974e35559645462975bcb882cde5
KC Barrett <kbarre123@gmail.com>
2013-12-04 15:02:10 -0600

    Testing vixen_WORKING_COPY

--- vixen/Vixen_WORKING_COPY_14_channels/Vixen_WORKING_COPY/Vixen_WORKING_COPY.ino
+++ vixen/Vixen_WORKING_COPY_14_channels/Vixen_WORKING_COPY/Vixen_WORKING_COPY.ino
@@ -16,21 +16,23 @@ its pins accordingly, which for testing purposes are represented by 14 LEDs.
 #define CHANNEL_10 11
 #define CHANNEL_11 12
 #define CHANNEL_12 13
-#define CHANNEL_13 A4
-#define CHANNEL_14 A5
+#define CHANNEL_13 A2
+#define CHANNEL_14 A3
+#define CHANNEL_15 A4
+#define CHANNEL_16 A5
 
 // Define array of channels.
 int channels[] = 
   {
     CHANNEL_01, CHANNEL_02, CHANNEL_03, CHANNEL_04, CHANNEL_05, CHANNEL_06, CHANNEL_07,
-    CHANNEL_08, CHANNEL_09, CHANNEL_10, CHANNEL_11, CHANNEL_12, CHANNEL_13, CHANNEL_14
+    CHANNEL_08, CHANNEL_09, CHANNEL_10, CHANNEL_11, CHANNEL_12, CHANNEL_13, CHANNEL_14, CHANNEL_15, CHANNEL_16
   };
 
 // Define number of channels.
-#define CHANNEL_COUNT 14
+#define CHANNEL_COUNT 16
 
 // Define buffer to store data from Vixen until it gets written to the Arduino.
-int incomingByte[14];
+int incomingByte[16];
 
 // Define the pin that will trigger "random mode".
 #define RANDOM_MODE_PIN A0
