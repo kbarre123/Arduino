7e073fc5eb4956e3bded419bf0dc0570f51750b9
KC Barrett <kbarre123@gmail.com>
2014-08-10 13:58:03 -0500

    Added copper cannon files

---
+++ copper_cannon/PIR_sensor_blake/PIR_sensor_blake.ino
@@ -0,0 +1,47 @@
+// Uses a PIR sensor to detect movement, buzzes a buzzer
+ 
+int ledPin = 7;                // choose the pin for the LED
+int inputPin = 6;               // choose the input pin (for PIR sensor)
+int pirState = LOW;             // we start, assuming no motion detected
+int val = 0;                    // variable for reading the pin status
+int pinSpeaker = 4;           //Set up a speaker on a PWM pin (digital 9, 10, or 11)
+
+void setup() {
+  pinMode(ledPin, OUTPUT);      // declare LED as output
+  pinMode(inputPin, INPUT);     // declare sensor as input
+  pinMode(pinSpeaker, OUTPUT);
+}
+
+void loop(){
+  val = digitalRead(inputPin);  // read input value
+  
+  if (val == HIGH) {            // check if the input is HIGH
+    digitalWrite(ledPin, HIGH); // turn LED ON
+    playTone(300, 160);
+    delay(150);
+    if (pirState == LOW) {
+      pirState = HIGH;          // update current state of PIR
+    }
+  } else {
+      digitalWrite(ledPin, LOW); // turn LED off
+      playTone(0, 0);
+      delay(300);    
+      if (pirState == HIGH){
+        pirState = LOW;        // update current state of PIR
+    }
+  }
+}
+//  duration in mSecs, frequency in hertz
+void playTone(long duration, int freq) {
+    duration *= 1000;
+    int period = (1.0 / freq) * 1000000;
+    long elapsed_time = 0;
+    while (elapsed_time < duration) {
+        digitalWrite(pinSpeaker,HIGH);
+        delayMicroseconds(period / 2);
+        digitalWrite(pinSpeaker, LOW);
+        delayMicroseconds(period / 2);
+        elapsed_time += (period);
+    }
+}
+
--- halloween/halloween.ino
+++ halloween/halloween.ino
@@ -1,44 +1,24 @@
-#include "pitches.h"
+#include "Pitches.h"
 
 //**************** TONE DECLARATIONS ****************
-int melody[] = {  // Notes in the melody:
-  NOTE_CS5, NOTE_FS4, NOTE_FS4, NOTE_CS5, NOTE_FS4, NOTE_FS4, NOTE_CS5, NOTE_FS4, NOTE_D5, NOTE_FS4,
-  NOTE_CS5, NOTE_FS4, NOTE_FS4, NOTE_CS5, NOTE_FS4, NOTE_FS4, NOTE_CS5, NOTE_FS4, NOTE_D5, NOTE_FS4, 
-  NOTE_CS5, NOTE_FS4, NOTE_FS4, NOTE_CS5, NOTE_FS4, NOTE_FS4, NOTE_CS5, NOTE_FS4, NOTE_D5, NOTE_FS4,
-  NOTE_CS5, NOTE_FS4, NOTE_FS4, NOTE_CS5, NOTE_FS4, NOTE_FS4, NOTE_CS5, NOTE_FS4, NOTE_D5, NOTE_FS4, 
-  
-  NOTE_C5, NOTE_F4, NOTE_F4, NOTE_C5, NOTE_F4, NOTE_F4, NOTE_C5, NOTE_F4, NOTE_CS5, NOTE_F4,
-  NOTE_C5, NOTE_F4, NOTE_F4, NOTE_C5, NOTE_F4, NOTE_F4, NOTE_C5, NOTE_F4, NOTE_CS5, NOTE_F4,
-  NOTE_C5, NOTE_F4, NOTE_F4, NOTE_C5, NOTE_F4, NOTE_F4, NOTE_C5, NOTE_F4, NOTE_CS5, NOTE_F4,
-  NOTE_C5, NOTE_F4, NOTE_F4, NOTE_C5, NOTE_F4, NOTE_F4, NOTE_C5, NOTE_F4, NOTE_CS5, NOTE_F4,
- };
+int melody[] = {
+  NOTE_CS5; 
+};
 
-int noteDurations[] = {  // Note durations: 4 = quarter note, 8 = eighth note, etc.:
-  6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 
-  6, 6, 6, 6, 6, 6, 6, 6, 6, 6,
-  6, 6, 6, 6, 6, 6, 6, 6, 6, 6,
-  6, 6, 6, 6, 6, 6, 6, 6, 6, 6,
-  
-  6, 6, 6, 6, 6, 6, 6, 6, 6, 6,
-  6, 6, 6, 6, 6, 6, 6, 6, 6, 6,
-  6, 6, 6, 6, 6, 6, 6, 6, 6, 6,
-  6, 6, 6, 6, 6, 6, 6, 6, 6, 6,
+int noteDurations[] = {
+  6 
 };
 
 //**************** LED DECLARATIONS *****************
-int ledPin1 = 3;      //Location of LED 1, eye1
-int ledPin2 = 13;     //Location of LED 2, eye2
-int ledPin3 = 1;      //Location of LED 3, ambient light
+int ledPin1 = 7;      //Location of LED 1, eye1
 
 //**************** PIR DECLARATIONS *****************
-int motionPin = 2;      //Input pin for PIR sensor
-int pirState = LOW;    // Initial state of PIR sensor
+int motionPin = 2;
+int pirState = LOW;
 int val = 0;           // Variable for reading PIR status
 
 void setup(){
   pinMode(ledPin1, OUTPUT);    // Initialize digital pin output
-  pinMode(ledPin2, OUTPUT);    // Initialize digital pin output
-  pinMode(ledPin3, OUTPUT);    // Initialize digital pin output
   pinMode(motionPin, INPUT);    // Declare sensor as input
 }
 
@@ -49,45 +29,23 @@ void loop() {
   if (val == HIGH) {
     
     // Iterate over the notes of the melody:
-    for (int thisNote = 0; thisNote < 80; thisNote++) {  
-    // to calculate the note duration, take one second 
-    // divided by the note type.
-    //e.g. quarter note = 1000 / 4, eighth note = 1000/8, etc.
-    int noteDuration = 1000/noteDurations[thisNote];
-    tone(4, melody[thisNote],noteDuration);
-
-    // to distinguish the notes, set a minimum time between them.
-    // the note's duration + 30% seems to work well:
-    int pauseBetweenNotes = noteDuration * 1.30;
-    delay(pauseBetweenNotes);
+    for (int thisNote = 0; thisNote < 1; thisNote++) {
+      // to calculate the note duration, take one second 
+      // divided by the note type.
+      //e.g. quarter note = 1000 / 4, eighth note = 1000/8, etc.
+      int noteDuration = 1000/noteDurations[thisNote];
+      tone(4, melody[thisNote],noteDuration);
     
-    // turn on green LED
-    digitalWrite(ledPin3, HIGH);
-    
-    // blink red LED's on alternating notes
-    if(thisNote % 2 == 0) {
+  
+      // to distinguish the notes, set a minimum time between them.
+      // the note's duration + 30% seems to work well:
+      int pauseBetweenNotes = noteDuration * 1.5;
+      delay(pauseBetweenNotes);
+      
       digitalWrite(ledPin1, HIGH);
-      digitalWrite(ledPin2, HIGH);
-    } else {
-        digitalWrite(ledPin1, LOW);
-        digitalWrite(ledPin2, LOW);
-      }
     }
-    
-    // Update current state of PIR
-    if (pirState == LOW) {         
-      pirState = HIGH;
-    }
-    
-  } else {
-    digitalWrite(ledPin1, HIGH);  
-    digitalWrite(ledPin2, HIGH);  
-    digitalWrite(ledPin3, HIGH);  
-    delay(300);
-    // Update current state of PIR
-    if(pirState == HIGH) {         
-      pirState = LOW;              
-    }
-  }
+  delay(1000);
+}
+el
 }
 
