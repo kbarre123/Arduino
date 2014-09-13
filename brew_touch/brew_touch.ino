4cdbb82e71e1178962056b73c5137f769fcc4889
KC Barrett <kbarre123@gmail.com>
2014-09-13 15:47:00 -0500

    Define colors, add displayTimer & updateTimer methods

--- brew_touch/brew_touch.ino
+++ brew_touch/brew_touch.ino
@@ -29,6 +29,10 @@ See the readme.txt file for information on find the libraries this library uses.
 #include <stdint.h>
 
 /******************* MENU SETUP ***********************/
+// Define colors
+#define WHITE TSC.createColor(255, 255, 255)
+#define BLACK TSC.createColor(0, 0, 0)
+
 // create the array of items for the main menu
 TouchScreenMenuItem mainMenuItems[] = {
   TouchScreenMenuItem("Water Chem"),
@@ -54,8 +58,8 @@ TouchScreenMenu mainMenu = TouchScreenMenu(mainMenuItems, 2, 20, 20, CENTERJ, "M
 TouchScreenMenu *curMenu = &mainMenu;
 
 // Buttons for non-menu screens that are only monitored if curMenu=NULL (i.e. not on a menu screen)
-TouchScreenArea backBtn =  TouchScreenButton("Back",  TSC.createColor(255, 255, 255), TSC.createColor(0, 0, 0), 20, TSC.getScreenHeight() - 50, 2, 10);
-TouchScreenArea resetBtn = TouchScreenButton("Reset", TSC.createColor(255, 255, 255), TSC.createColor(0, 0, 0), 125, TSC.getScreenHeight() - 50, 2, 10);
+TouchScreenArea backBtn =  TouchScreenButton("Back",  WHITE, BLACK, 20, TSC.getScreenHeight() - 50, 2, 10);
+TouchScreenArea resetBtn = TouchScreenButton("Reset", WHITE, BLACK, 125, TSC.getScreenHeight() - 50, 2, 10);
 
 /******************* TEMP SENSOR SETUP *******************/
 float tempUpper = 0;
@@ -71,11 +75,12 @@ const long MILLIS_IN_MINUTE = 60000;  // Constant to calculate printable time
 char _hour[3]; // Buffer to store the current hour in. Used to compare to see if it has changed and needs to be updated. Prevents blinking every pass through main loop.
 char _minute[3];
 char _second[3];
+boolean displayTimerSwitch = false;
 
 void setup(void) {
   Serial.begin(9600);
   previousMillis = millis();
-  TSC.setBackColor(TSC.createColor(0, 0, 0)); // change the default background color
+  TSC.setBackColor(BLACK); // change the default background color
   TSC.init(); // make sure everything get initialized
   curMenu->draw(); // put up the main menu
 }
@@ -91,6 +96,10 @@ void loop(void) {
     // if there isn't a current menu being displayed check all of the buttons
     // to see if any of them were pressed
     checkButtons();
+  }
+  if(displayTimerSwitch == true)
+  {
+    displayTimer();
   }
 }
 
@@ -118,17 +127,21 @@ void checkMenuSelection(TouchScreenMenuItem *item) {
         dtostrf(tempMash, 1, 2, textMash);
         
         // Display results
-        TSC.drawString("Upper:", 20, 20, 2, TSC.createColor(255, 255, 255));
-        TSC.drawString(textUpper, 150, 20, 2, TSC.createColor(255, 255, 255));
+        TSC.drawString("Upper:", 20, 20, 2, WHITE);
+        TSC.drawString(textUpper, 150, 20, 2, WHITE);
         Serial.print("textUpper: ");  // DEBUG
         Serial.println(textUpper);  // DEBUG
         
-        TSC.drawString("Mash:", 20, 50, 2, TSC.createColor(255, 255, 255));
-        TSC.drawString(textMash, 150, 50, 2, TSC.createColor(255, 255, 255));
+        TSC.drawString("Mash:", 20, 50, 2, WHITE);
+        TSC.drawString(textMash, 150, 50, 2, WHITE);
         Serial.print("textMash: ");  // DEBUG
         Serial.println(textMash);  // DEBUG
         Serial.println("");  // DEBUG
         
+        // Display updated timer on LCD
+        displayTimerSwitch = true;
+        
+        // Draw buttons
         backBtn.draw();
         resetBtn.draw();
         handled = true;
@@ -145,13 +158,13 @@ void checkMenuSelection(TouchScreenMenuItem *item) {
         curMenu = NULL;
         TSC.clearScreen();
         int yPos = 20;
-        TSC.drawString("Pale Ale    5gal  10gal", 20, yPos,       1, TSC.createColor(255, 255, 255));
-        TSC.drawString("Dilution:    50%    50%", 20, yPos += 20, 1, TSC.createColor(255, 255, 255));
-        TSC.drawString("Gypsum:     7.0g  14.0g", 20, yPos += 20, 1, TSC.createColor(255, 255, 255));
-        TSC.drawString("Salt:       1.2g   2.4g", 20, yPos += 20, 1, TSC.createColor(255, 255, 255));
-        TSC.drawString("CaCl2:      1.0g   2.0g", 20, yPos += 20, 1, TSC.createColor(255, 255, 255));
-        TSC.drawString("Pick Lime:  0.2g   0.4g", 20, yPos += 20, 1, TSC.createColor(255, 255, 255));
-        TSC.drawString("MgCl2:      1.0g   2.0g", 20, yPos += 20, 1, TSC.createColor(255, 255, 255));
+        TSC.drawString("Pale Ale    5gal  10gal", 20, yPos,       1, WHITE);
+        TSC.drawString("Dilution:    50%    50%", 20, yPos += 20, 1, WHITE);
+        TSC.drawString("Gypsum:     7.0g  14.0g", 20, yPos += 20, 1, WHITE);
+        TSC.drawString("Salt:       1.2g   2.4g", 20, yPos += 20, 1, WHITE);
+        TSC.drawString("CaCl2:      1.0g   2.0g", 20, yPos += 20, 1, WHITE);
+        TSC.drawString("Pick Lime:  0.2g   0.4g", 20, yPos += 20, 1, WHITE);
+        TSC.drawString("MgCl2:      1.0g   2.0g", 20, yPos += 20, 1, WHITE);
         backBtn.draw();
         handled = true;
       }
@@ -159,13 +172,13 @@ void checkMenuSelection(TouchScreenMenuItem *item) {
         curMenu = NULL;
         TSC.clearScreen();
         int yPos = 20;
-        TSC.drawString("Amber/ESB    5gal   10gal", 20, yPos,       1, TSC.createColor(255, 255, 255));
-        TSC.drawString("Dilution:      0%      0%", 20, yPos += 20, 1, TSC.createColor(255, 255, 255));
-        TSC.drawString("Gypsum:     1.80g   3.60g", 20, yPos += 20, 1, TSC.createColor(255, 255, 255));
-        TSC.drawString("Salt:       0.00g   0.00g", 20, yPos += 20, 1, TSC.createColor(255, 255, 255));
-        TSC.drawString("CaCl2:      1.20g   2.40g", 20, yPos += 20, 1, TSC.createColor(255, 255, 255));
-        TSC.drawString("Phos Acid:  0.80g   1.60g", 20, yPos += 20, 1, TSC.createColor(255, 255, 255));
-        TSC.drawString("MgCl2:      1.12g   2.24g", 20, yPos += 20, 1, TSC.createColor(255, 255, 255));
+        TSC.drawString("Amber/ESB    5gal   10gal", 20, yPos,       1, WHITE);
+        TSC.drawString("Dilution:      0%      0%", 20, yPos += 20, 1, WHITE);
+        TSC.drawString("Gypsum:     1.80g   3.60g", 20, yPos += 20, 1, WHITE);
+        TSC.drawString("Salt:       0.00g   0.00g", 20, yPos += 20, 1, WHITE);
+        TSC.drawString("CaCl2:      1.20g   2.40g", 20, yPos += 20, 1, WHITE);
+        TSC.drawString("Phos Acid:  0.80g   1.60g", 20, yPos += 20, 1, WHITE);
+        TSC.drawString("MgCl2:      1.12g   2.24g", 20, yPos += 20, 1, WHITE);
         backBtn.draw();
         handled = true;
       }
@@ -173,13 +186,13 @@ void checkMenuSelection(TouchScreenMenuItem *item) {
         curMenu = NULL;
         TSC.clearScreen();
         int yPos = 20;
-        TSC.drawString("Brn/Prtr     5gal   10gal", 20, yPos,       1, TSC.createColor(255, 255, 255));
-        TSC.drawString("Dilution:     50%     50%", 20, yPos += 20, 1, TSC.createColor(255, 255, 255));
-        TSC.drawString("Gypsum:     1.80g   3.60g", 20, yPos += 20, 1, TSC.createColor(255, 255, 255));
-        TSC.drawString("Pick Lime:  0.40g   0.80g", 20, yPos += 20, 1, TSC.createColor(255, 255, 255));
-        TSC.drawString("CaCl2:      0.80g   1.60g", 20, yPos += 20, 1, TSC.createColor(255, 255, 255));
-        TSC.drawString("Phos Acid:  0.80g   1.60g", 20, yPos += 20, 1, TSC.createColor(255, 255, 255));
-        TSC.drawString("MgCl2:      1.12g   2.24g", 20, yPos += 20, 1, TSC.createColor(255, 255, 255));
+        TSC.drawString("Brn/Prtr     5gal   10gal", 20, yPos,       1, WHITE);
+        TSC.drawString("Dilution:     50%     50%", 20, yPos += 20, 1, WHITE);
+        TSC.drawString("Gypsum:     1.80g   3.60g", 20, yPos += 20, 1, WHITE);
+        TSC.drawString("Pick Lime:  0.40g   0.80g", 20, yPos += 20, 1, WHITE);
+        TSC.drawString("CaCl2:      0.80g   1.60g", 20, yPos += 20, 1, WHITE);
+        TSC.drawString("Phos Acid:  0.80g   1.60g", 20, yPos += 20, 1, WHITE);
+        TSC.drawString("MgCl2:      1.12g   2.24g", 20, yPos += 20, 1, WHITE);
         backBtn.draw();
         handled = true;
       }
@@ -187,14 +200,14 @@ void checkMenuSelection(TouchScreenMenuItem *item) {
         curMenu = NULL;
         TSC.clearScreen();
         int yPos = 20;
-        TSC.drawString("Stout       5gal  10gal", 20, yPos,       1, TSC.createColor(255, 255, 255));
-        TSC.drawString("Dilution:    25%    25%", 20, yPos += 20, 1, TSC.createColor(255, 255, 255));
-        TSC.drawString("Gypsum:     0.8g   1.6g", 20, yPos += 20, 1, TSC.createColor(255, 255, 255));
-        TSC.drawString("Salt:       0.4g   0.8g", 20, yPos += 20, 1, TSC.createColor(255, 255, 255));
-        TSC.drawString("CaCl2:      0.4g   0.8g", 20, yPos += 20, 1, TSC.createColor(255, 255, 255));
-        TSC.drawString("Phos Acid:  1.0g   2.0g", 20, yPos += 20, 1, TSC.createColor(255, 255, 255));
-        TSC.drawString("Pick Lime:  0.8g   1.6g", 20, yPos += 20, 1, TSC.createColor(255, 255, 255));
-        TSC.drawString("MgCl2:      1.0g   2.0g", 20, yPos += 20, 1, TSC.createColor(255, 255, 255));
+        TSC.drawString("Stout       5gal  10gal", 20, yPos,       1, WHITE);
+        TSC.drawString("Dilution:    25%    25%", 20, yPos += 20, 1, WHITE);
+        TSC.drawString("Gypsum:     0.8g   1.6g", 20, yPos += 20, 1, WHITE);
+        TSC.drawString("Salt:       0.4g   0.8g", 20, yPos += 20, 1, WHITE);
+        TSC.drawString("CaCl2:      0.4g   0.8g", 20, yPos += 20, 1, WHITE);
+        TSC.drawString("Phos Acid:  1.0g   2.0g", 20, yPos += 20, 1, WHITE);
+        TSC.drawString("Pick Lime:  0.8g   1.6g", 20, yPos += 20, 1, WHITE);
+        TSC.drawString("MgCl2:      1.0g   2.0g", 20, yPos += 20, 1, WHITE);
         backBtn.draw();
         handled = true;
       }
@@ -202,7 +215,7 @@ void checkMenuSelection(TouchScreenMenuItem *item) {
     
     // if the menu item didn't get handled redraw it unpressed
     if(handled==false)
-        curMenu->drawItem(item,false);
+      curMenu->drawItem(item,false);
     }
 }
 
@@ -210,6 +223,7 @@ void checkMenuSelection(TouchScreenMenuItem *item) {
 void checkButtons(){
   if(backBtn.process()){ // return from the graphics function screen
     curMenu = &waterMenu;
+    displayTimerSwitch == false;
     TSC.clearScreen();
     curMenu->draw();
   }
@@ -217,4 +231,63 @@ void checkButtons(){
     // Reset timer
   }
 }
+
+void updateTimer()
+{
+  // Check to see if 1 second has elapsed
+  currentMillis = millis();
+  if(currentMillis - previousMillis > interval) 
+  {
+    // Save the last time you updated Timer
+    previousMillis = currentMillis;
+    
+    /*** UPDATE TIMER COMPONENTS ***/
+    if (currentMillis > benchMillis + MILLIS_IN_MINUTE) 
+    {
+      minute++;
+      second = 0;
+      benchMillis = currentMillis;
+    }
+    else
+    {
+      second = (currentMillis - benchMillis) / 1000;
+    }
+    if (minute > 59) 
+    {
+      hour++;
+      minute = 0;
+    }
+  }
+}
+
+void displayTimer()
+{
+  // Print ints to string in preparation of printing to LCD display
+  sprintf(_hour, "%02d", hour); // Arguments are: (buffer, format, value to print)
+  Serial.print(_hour);
+  Serial.print(":");
+  
+  sprintf(_minute, "%02d", minute); 
+  Serial.print(_minute);
+  Serial.print(":");  
+  
+  sprintf(_second, "%02d", second);
+  Serial.println(_second);
+  Serial.println("");
+  
+  // Display text
+  TSC.drawString(_hour, 20, 80, 2, WHITE);
+  Tft.drawString(":", 50, 80, 2, WHITE);
+  
+  Tft.drawString(_minute, 65, 80, 2, WHITE);
+  Tft.drawString(":", 95, 80, 2, WHITE);
+  
+  Tft.drawString(_second, 110, 80, 2, WHITE);
+  
+  // Erase text
+  Tft.drawString(_hour, 20, 80, 2, BLACK);
+  Tft.drawString(_minute, 65, 80, 2, BLACK);
+  Tft.drawString(_second, 110, 80, 2, BLACK);
+}
+
 
