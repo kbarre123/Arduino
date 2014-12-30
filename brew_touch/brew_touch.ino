ba677704f174a0e0ff831394624a7c1171e5db32
KC Barrett <kbarre123@gmail.com>
2014-12-29 20:06:26 -0600

    Print temps to LCD

--- brew_touch/brew_touch.ino
+++ brew_touch/brew_touch.ino
@@ -6,14 +6,6 @@
  * Arduino Mega
  *******************************************************************/
 
- /******************************************************************
- * Connections for sensors:
- * DS18B20 Pinout:
- * - Black   = Ground
- * - Orange  = Signal (with 3.3K to 4.7K resistor to +5 or 3.3 )
- * - Red     = +5 or +3.3 V                                               
- *******************************************************************/
-
 #include <stdint.h>
 #include <TouchScreen.h> // Will be used to read touch events
 #include <OneWire.h> // Get 1-wire Library here: http://www.pjrc.com/teensy/td_libs_OneWire.html
@@ -41,6 +33,12 @@
 
 /********************************************************************
 * Set up oneWire sensors and pass them to the Dallas Temp library
+ 
+ * Connections for sensors:
+ * DS18B20 Pinout:
+ * - Black   = Ground
+ * - Orange  = Signal (with 4.7K pull-up resistor to +5V )
+ * - Red     = +5V (with 4.7K pull-up resistor to Signal )        
 ********************************************************************/
 #define ONE_WIRE_BUS_PIN 22
 OneWire oneWire(ONE_WIRE_BUS_PIN); // Setup a oneWire instance to communicate with any OneWire devices
@@ -58,8 +56,14 @@ DeviceAddress ProbeB = {
 * Global Variables
 ********************************************************************/
 unsigned long sysTime;
-//float tempUpper = 0;
-//float tempMash = 0;
+float tempA = 0;  // stores results of reading the sensor
+float tempB = 0;
+float _tempA = 0; // stores the old result of tempA for updating LCD screen
+float _tempB = 0;
+char textA[8];    // buffer to store the results of dtostrf TODO: refactor so this isn't global
+char textB[8];
+char _textA[8];   // stores the old result of textA for updating LCD screen
+char _textB[8];
 
 /********************************************************************
 * Set Up
@@ -70,8 +74,8 @@ void setup()
   Tft.init();  //init TFT library
   Tft.paintScreenBlack();  // Clear screen
   Tft.setDisplayDirect(UP2DOWN);
-  Tft.drawString("Upper: ",220,20,2,WHITE);
-  Tft.drawString("Mash: ",180,20,2,WHITE);
+  Tft.drawString("A:",180,40,3,WHITE);
+  Tft.drawString("B:",90,40,3,WHITE);
   Serial.print("Initializing Temperature Control Library Version ");
   Serial.println(DALLASTEMPLIBVERSION);
   sensors.begin(); // Initialize the Temperature measurement library
@@ -92,48 +96,64 @@ void loop()
   delay(1000);
   sensors.requestTemperatures(); // Command all devices on bus to read temperature
   Serial.print("Probe A:\t");
-  printTemperature(ProbeA);
-  Serial.println();
+  getTemperature(ProbeA, tempA);
   Serial.print("Probe B:\t");
-  printTemperature(ProbeB);
+  getTemperature(ProbeB, tempB);
+  /*Serial.println(""); // DEBUG
+  Serial.print("tempA: "); // DEBUG
+  Serial.println(tempA); // DEBUG
+  Serial.print("tempB: "); // DEBUG
+  Serial.println(tempB); // DEBUG */
   Serial.println("\n\r");
 
-  // OLD CODE HERE; DON'T DELETE YET!!!
-  /* Convert sensor data of Upper Pot to string for display. 
-  char textUpper[8]; // buffer to store the results of dtostrf
-  dtostrf(tempUpper, 1, 2, textUpper);  // Arguments are (float, width, precision, buffer)
-  // Convert sensor data of Mash Tun to string for display
-  char textMash[8];  // buffer to store the results of dtostrf
-  dtostrf(tempMash, 1, 2, textMash);
-  // Display results
-  Tft.drawString(textUpper,220,180,2,WHITE);
-  //Serial.print("textUpper: ");  // DEBUG
-  //Serial.println(textUpper);  // DEBUG
-  Tft.drawString(textMash,180,180,2,WHITE);
-  //Serial.print("textMash: ");  // DEBUG
-  //Serial.println(textMash);  // DEBUG
-  //Serial.println("");  // DEBUG
-  */
+  /* Convert floats to strings for LCD screen */
+  dtostrf(tempA, 1, 2, textA); // Arguments are (float, width, precision, buffer)
+  dtostrf(tempB, 1, 2, textB); // Arguments are (float, width, precision, buffer)
+  /*Serial.print("textA: "); // DEBUG
+  Serial.println(textA); // DEBUG
+  Serial.print("textB: "); // DEBUG
+  Serial.println(textB); // DEBUG
+  Serial.println(""); // DEBUG */
+
+  /* Display results on LCD screen only if they've changed */
+  if (tempA != _tempA)
+  {
+    Tft.drawString(_textA,180,120,4,BLACK);
+    Tft.drawString(textA,180,120,4,WHITE);
+  }
+
+  if (tempB != _tempB)
+  {
+    Tft.drawString(_textB,90,120,4,BLACK);
+    Tft.drawString(textB,90,120,4,WHITE);
+  }
+
+  // Store current results for comparison
+  _tempA = tempA;
+  _tempB = tempB;
+  strncpy(_textA, textA, 8);
+  strncpy(_textB, textB, 8);
 }
 
 /*******************************************************************
 * User Defined Functions
 ********************************************************************/
-void printTemperature(DeviceAddress deviceAddress)
+// This gets the temp, prints it to Serial, and assigns it to its
+// global variable (for future printing to LCD screen)
+void getTemperature(DeviceAddress deviceAddress, float& sensorName)
 {
 
-  float tempC = sensors.getTempC(deviceAddress);
+  float tempF = sensors.getTempF(deviceAddress);
+  sensorName = tempF;
 
-  if (tempC == -127.00) 
+  if (tempF < -100.00) 
   {
-    Serial.print("Error getting temperature  ");
-  } 
+    Serial.print("Error getting temperature");
+  }
   else
   {
-    Serial.print("C: ");
-    Serial.print(tempC);
-    Serial.print("\tF: ");
-    Serial.print(DallasTemperature::toFahrenheit(tempC));
+    Serial.print("F: ");
+    Serial.print(tempF);
     Serial.print("\tTime: ");
     sysTime = millis();
     Serial.println(sysTime / 1000);
@@ -144,7 +164,7 @@ void printTemperature(DeviceAddress deviceAddress)
   Number of devices found on bus = 2
   Getting temperatures...
 
-  Probe A:  C: 24.75  F: 76.55  Time: 4
-  Probe B:  C: 24.00  F: 75.20  Time: 4
+  Probe A:  F: 76.55  Time: 4
+  Probe B:  F: 75.20  Time: 4
   */
 }
