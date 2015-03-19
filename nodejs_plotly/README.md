f60bd23f7a7a5c90291ebc6116716e5103f0e6cc
KC Barrett <kbarre123@gmail.com>
2015-03-19 14:11:15 -0500

    Add new projects from devo

---
+++ nodejs_plotly/README.md
@@ -0,0 +1,5 @@
+Notes:
+
+The node modules are shared by tmp36.js and brew_temp.js. I need to merge plotly stuff from tmp36.js into brew_temp.js.
+
+Note that the node_modules folder is included in .gitignore. When cloning this project, be sure to run `npm install plotly` and 'npm install johnny-five' inside of /nodejs_plotly folder.
---
+++ nodejs_plotly/tmp36.js
@@ -0,0 +1,56 @@
+var plotly = require('plotly')('kbarre123', 'g5nzo225vi');
+var five = require("johnny-five");
+var brew_bot = new five.Board();
+
+var data = [{x:[], y:[], stream:{token:'wboncpxs1m', maxpoints:100}}];
+var layout = {filename : "Arduino Temp Stream"};
+
+brew_bot.on("ready", function() {
+
+  // create a new tmp36 sensor object
+  var tmp36 = new five.Sensor({
+    pin: "A0",
+    freq: 1000, // get reading every 1000ms
+    thresh: 0.5
+  });
+  
+  // initialize the plotly graph
+  plotly.plot(data,layout,function (err, res) {
+    if (err) console.log(err);
+    console.log(res);
+    //once it's initialized, create a plotly stream
+    //to pipe your data!
+    var stream = plotly.stream('wboncpxs1m', function (err, res) {
+      if (err) console.log(err);
+      console.log(res);
+    });
+    // this gets called each time there is a new sensor reading
+    tmp36.on("data", function() {
+      var data = {
+        x : getDateString(),
+        y : convertTemperature(this.value)
+      };
+      console.log(data);
+      // write the data to the plotly stream
+      stream.write(JSON.stringify(data)+'\n');
+    });
+  });
+});
+
+// helper function to convert sensor value to temp
+function convertTemperature (value) {
+  var voltage = value * 0.004882814;
+  var celsius = (voltage - 0.5) * 100;
+  var fahrenheit = celsius * (9 / 5) + 32;
+  return celsius;
+}
+
+// little helper function to get a nicely formatted date string
+function getDateString () {
+  var time = new Date();
+  // 14400000 is (GMT-4 Montreal)
+  // for your timezone just multiply +/-GMT by 3600000
+  var datestr = new Date(time - 18000000).toISOString().replace(/T/, ' ').replace(/Z/, '');
+  return datestr;
+}
+
