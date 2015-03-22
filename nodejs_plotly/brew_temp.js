d1180ac41755e70630b211ebeecb0b94387680fe
KC Barrett <kbarre123@gmail.com>
2015-03-22 14:11:52 -0500

    Still works

--- nodejs_plotly/brew_temp.js
+++ nodejs_plotly/brew_temp.js
@@ -16,101 +16,70 @@ var board = new five.Board();
 // the pin the DS18B20 is connected on
 var pin = 2;
 
-// Plotly stuff
 var data = [{
   x:[], 
-  y:[], 
+  y:[],
+  name: 'Top',
   stream:{
-    token:'wboncpxs1m', 
-    maxpoints:1800
+    token:'0om2z8lncl', 
+    maxpoints:200
   }
 }];
 
 var layout = { 
-  title: "Arduino Temp Stream (LM35)",
+  title: "Arduino Temp Stream (DS18B20)",
   xaxis: {
     title: "Datetime"
   },
   yaxis: {
     title: "Temperature (*F)"
+  },
+  autosize: true,
+  showlegend: true,
+  legend: {
+    x: 100,
+    y: 1
   }
 };
 
 var graphOptions = {
   layout: layout,
   fileopt: "overwrite", 
-  filename : "Arduino Temp Stream (LM35)",
+  filename : "Arduino Temp Stream (DS18B20)",
 }; // End Plotly stuff
 
 board.on('ready', function () {
+  // This requires OneWire support using the ConfigurableFirmata
+  var temperature = new five.Temperature({
+    controller: "DS18B20",
+    pin: 2,
+    freq: 1000
+  });
 
-  board.firmata = board.io;
-  board.firmata.sendOneWireConfig(pin, true);
-  board.firmata.sendOneWireSearch(pin, function(error, devices) {
-    if(error) {
-      console.error(error);
-      return;
-    }
-
-    // only interested in the first device
-    var device = devices[0];
-
-    var readTemperature = function() {
-      // led.on();
-
-      // start transmission
-      board.firmata.sendOneWireReset(pin);
-
-      // a 1-wire select is done by ConfigurableFirmata
-      board.firmata.sendOneWireWrite(pin, device, 0x44);
-
-      // the delay gives the sensor time to do the calculation
-      board.firmata.sendOneWireDelay(pin, 1000);
-
-      // start transmission
-      board.firmata.sendOneWireReset(pin);
-
-      // tell the sensor we want the result and read it from the scratchpad
-      board.firmata.sendOneWireWriteAndRead(pin, device, 0xBE, 9, function(error, data) {
-        if(error) {
-          console.error(error);
-          return;
-        }
-        var raw = (data[1] << 8) | data[0];
-        var celsius = raw / 16.0;
-        var fahrenheit = celsius * 1.8 + 32.0;
-        return fahrenheit;
-      });
-    };
-    // initialize the plotly graph
-    plotly.plot(data, graphOptions, function (err, res) {
+  // initialize the plotly graph
+  plotly.plot(data, graphOptions, function (err, res) {
+    if (err) console.log(err);
+    console.log(res);
+    //once it's initialized, create a plotly stream
+    //to pipe your data!
+    var stream = plotly.stream('0om2z8lncl', function (err, res) {
       if (err) console.log(err);
       console.log(res);
-      //once it's initialized, create a plotly stream
-      //to pipe your data!
-      var stream = plotly.stream('wboncpxs1m', function (err, res) {
-        if (err) console.log(err);
-        console.log(res);
-      });
-      // this gets called each time there is a new sensor reading
-      readTemperature().on("data", function() {
-        var data = {
-          x : getDateString(),
-          y : this.value
-        };
-        console.log(data);
-        // write the data to the plotly stream
-        stream.write(JSON.stringify(data)+'\n');
-      });
     });
-    // read the temperature now
-    //readTemperature();
-    // and every 1 second
-    //setInterval(readTemperature, 1000);
-  });
-});
+    temperature.on("data", function(err, data) {
+      if (err) console.log(err);
+      var data = {
+        x : getDateString(),
+        y : data.fahrenheit
+      };
+      //console.log(data.celsius + "°C", data.fahrenheit + "°F");
+      console.log(data);
+      stream.write(JSON.stringify(data)+'\n');
+    });
+  }); // End Plotly
+}); // End board.on
 
-// little helper function to get a nicely formatted date string
+// helper function to get a nicely formatted date string
 function getDateString () {
   var time = new Date();
   // 14400000 is (GMT-4 Montreal)
