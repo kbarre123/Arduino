e6f92e5874775c4d78caf8098b673aaab696c7d7
KC Barrett <kbarre123@gmail.com>
2015-03-31 11:16:57 -0500

    Expose device address in cosole

--- nodejs_plotly/device_diagnostics.js
+++ nodejs_plotly/device_diagnostics.js
@@ -5,30 +5,48 @@ var board = new five.Board();
 // the pin the DS18B20 is connected on
 var pin = 2;
 
-var data = [{
-  x:[]
-}];
-
 // Don't do anything until the board is ready for communication.
 board.on('ready', function () {
   // This requires OneWire support using the ConfigurableFirmata
-  var temperature = new five.Temperature({
+  var temperature_1 = new five.Temperature({
     controller: "DS18B20",
-    address: 0x60598c0,
+    //address: 0x60630e7, // Probe A
     pin: pin,
+    bus_pos: 1,
     freq: 1000
   });
+  
+  /*var temperature_2 = new five.Temperature({
+    controller: "DS18B20",
+    //address: 0x60598c0, // Probe B
+    //pin: pin,
+    bus_pos: 1,
+    freq: 1000
+  });*/
+  
   // Announce all devices on bus
-  temperature.once("data", function() {
-    //console.log(temperature);
+  temperature_1.once("data", function() {
     console.log('Address', this.address.toString(16));
   });
 
-  temperature.on("data", function(err, data) {
+  /*temperature_2.once("data", function() {
+    console.log('Address', this.address.toString(16));
+  });*/
+
+  temperature_1.on("data", function(err, data) {
     if (err) console.log(err);
     var data = {
       x : data.fahrenheit
     };
     console.log(data);
   });
+
+  /*temperature_2.on("data", function(err, data) {
+    if (err) console.log(err);
+    var data = {
+      x : data.fahrenheit
+    };
+    console.log('Sensor 2', data);
+  });*/
+
 }); // End board.on
